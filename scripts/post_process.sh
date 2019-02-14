4e0fa1cb391d278af0a05bd3ba1a27cbbdc8b63d
shawnan <shawnan.chen@gmail.com>
2019-02-14 22:53:58 +0800

    change script

--- scripts/post_process.sh
+++ scripts/post_process.sh
@@ -3,12 +3,19 @@
 # post process script
 # first param: hexo workspace path
 
-POSTS_PATH=$1
-echo $POSTS_PATH
-rm -rf $POSTS_PATH/source/_posts/*
-cp -f ./posts/* $POSTS_PATH/source/_posts
+BLOG_SOURCE_PATH=$1
+HTML_PATH=$2
 
-cd $POSTS_PATH
+echo $BLOG_SOURCE_PATH  $HTML_PATH
+
+rm -rf $BLOG_SOURCE_PATH/source/_posts/*
+cp -f ./posts/* $BLOG_SOURCE_PATH/source/_posts
+
+cd $BLOG_SOURCE_PATH
 source /etc/profile
 hexo clean
 hexo g
+
+rm -rf $HTML_PATH/blog/*
+cp $BLOG_SOURCE_PATH/public/* $HTML_PATH/blog/*
+

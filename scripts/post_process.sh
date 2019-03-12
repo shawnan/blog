501c1abc086103d3f17d0977938ac06eeda2d01a
shawnan <shawnan.chen@gmail.com>
2019-03-12 22:30:36 +0800

    增加脑图页面

--- scripts/post_process.sh
+++ scripts/post_process.sh
@@ -11,6 +11,8 @@ echo $BLOG_SOURCE_PATH  $HTML_PATH $NGINX_PATH
 
 # 放置nginx的首页
 cp -rf htmls/index.html $NGINX_PATH/html/nginx_index.html
+cp -rf htmls/frontend-mind-map.html $NGINX_PATH/html/mindmap.html
+cp -rf resources/* $NGINX_PATH/html/resources/
 
 rm -rf $BLOG_SOURCE_PATH/source/_posts/*
 cp -f ./posts/* $BLOG_SOURCE_PATH/source/_posts
